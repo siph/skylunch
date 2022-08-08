d9768abca2b70eb6d33f414dc36837ed24b5dc25
siph <dawkins.chris.dev@gmail.com>
2022-08-08 17:17:01 -0600

    init

---
+++ src/main/kotlin/com/skylunch/skylunch/SkylunchApplication.kt
@@ -0,0 +1,11 @@
+package com.skylunch.skylunch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkylunchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SkylunchApplication>(*args)
+}
---
+++ src/test/kotlin/com/skylunch/skylunch/SkylunchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.skylunch.skylunch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkylunchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
