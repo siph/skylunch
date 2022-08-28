f6c3628f5ac3f81715b7741bfdea13cd4298ea78
siph <dawkins.chris.dev@gmail.com>
2022-08-28 07:49:18 -0600

    feature: security configuration
    This is very simple security configuration which allows all requests
    to the search endpoint and disable csrf.

---
+++ src/main/kotlin/com/skylunch/security/SecurityConfigurations.kt
@@ -0,0 +1,26 @@
+package com.skylunch.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+class SecurityConfigurations {
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .authorizeExchange()
+            .pathMatchers("/api/v1/search")
+            .permitAll()
+            .and()
+            .csrf().disable()
+            .build()
+    }
+}
