a214963e9a40bd11e7ee11f52dd1e7029b73acbd
siph <dawkins.chris.dev@gmail.com>
2022-09-09 16:40:24 -0600

    ci: use remote database in docker deployment.
    Temporarily use the cloud based database instead of the docker
    container.

--- start.sh
+++ start.sh
@@ -8,6 +8,12 @@ rapidApiRootUrl=https://airports-by-api-ninjas.p.rapidapi.com/v1/airports
 googleApiKey=key
 googleApiRootUrl=https://maps.googleapis.com/maps/api/place
 
+# Redis
+redisHost=localhost
+redisPort=0
+redisUsername=username
+redisPassword=password
+
 # Airport
 export APPLICATION_AIRPORT_DAYSUNTILSTALE=0
 export APPLICATION_AIRPORT_API_APIKEY=$rapidApiKey
@@ -19,12 +25,14 @@ export APPLICATION_RESTAURANT_API_APIKEY=$googleApiKey
 export APPLICATION_RESTAURANT_API_BASEURL=$googleApiRootUrl
 
 # Redis
-export SPRING_REDIS_HOST=localhost
-export SPRING_REDIS_PORT=6379
+export SPRING_REDIS_HOST=$redisHost
+export SPRING_REDIS_PORT=$redisPort
+export SPRING_REDIS_USERNAME=$redisUsername
+export SPRING_REDIS_PASSWORD=$redisPassword
 
 # Server
 export SERVER_PORT=8080
-export LOGGING_LEVEL_ROOT=INFO
+export LOGGING_LEVEL_ROOT=DEBUG
 
 # Run
 docker-compose up -d
