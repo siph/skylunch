a5c7f489cb1b4bf7a1a722e4056ee2238fe3fb40
siph <dawkins.chris.dev@gmail.com>
2023-02-14 09:59:06 -0700

    test: restaurant service branch coverage

--- src/test/kotlin/com/skylunch/restaurant/RestaurantServiceTests.kt
+++ src/test/kotlin/com/skylunch/restaurant/RestaurantServiceTests.kt
@@ -5,6 +5,10 @@ import com.skylunch.AbstractBaseDocumentTest
 import com.skylunch.getMockCandidatesDTO
 import com.skylunch.getMockResponseOK
 import com.skylunch.getMockRestaurantProperties
+import com.skylunch.restaurant.restaurantApi.Geometry
+import com.skylunch.restaurant.restaurantApi.LatLngLiteral
+import com.skylunch.restaurant.restaurantApi.Place
+import com.skylunch.restaurant.restaurantApi.RestaurantApiDTO
 import com.skylunch.restaurant.restaurantApi.RestaurantApiService
 import okhttp3.mockwebserver.MockWebServer
 import org.assertj.core.api.Assertions.assertThat
@@ -73,6 +77,25 @@ class RestaurantServiceTests : AbstractBaseDocumentTest() {
             .filter { it.id == restaurant.id }
             .blockFirst()!!
         assertThat(updatedRestaurant.modified).isAfter(staleDate)
+    }
+
+    @Test
+    fun `assert new restaurants are cached`() {
+        val place = Place(
+            address = "address",
+            phoneNumber = "1234",
+            name = "spots",
+            rating = "4",
+            totalRating = "13",
+            url = "b.c",
+            website = "google.com",
+            geometry = Geometry(LatLngLiteral(-118.4079971, 33.94250107))
+        )
+        val json = ObjectMapper().writeValueAsString(RestaurantApiDTO(results = listOf(place), status = "OK"))
+        val mockResponse = getMockResponseOK(json)
+        server.enqueue(mockResponse)
+        restaurantService.getRestaurants(Point(-118.4079971, 33.94250107)).blockFirst()!!
+        assertThat(restaurantRepository.findAll().size).isEqualTo(1)
     }
 
     private fun getMockRestaurant(
