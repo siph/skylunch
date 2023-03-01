998685178297bb3aa6acaf73fb9c70459da44777
siph <dawkins.chris.dev@gmail.com>
2023-02-28 17:07:25 -0700

    refactor: type conversion

--- src/main/kotlin/com/skylunch/http/SearchDTO.kt
+++ src/main/kotlin/com/skylunch/http/SearchDTO.kt
@@ -1,6 +1,7 @@
 package com.skylunch.http
 
 import com.skylunch.restaurant.Restaurant
+import org.springframework.data.geo.Point
 
 data class SearchDTO(
     val name: String?,
@@ -18,6 +19,13 @@ data class Coords(
     val longitude: String
 )
 
+fun Point.toCoords(): Coords {
+    return Coords(
+        latitude = this.y.toString(),
+        longitude = this.x.toString()
+    )
+}
+
 fun Restaurant.toSearchDTO(): SearchDTO {
     return SearchDTO(
         name = this.name,
@@ -27,9 +35,6 @@ fun Restaurant.toSearchDTO(): SearchDTO {
         url = this.url,
         totalRating = this.totalRating,
         website = this.website,
-        coords = Coords(
-            latitude = this.location.y.toString(),
-            longitude = this.location.x.toString()
-        )
+        coords = this.location.toCoords()
     )
 }
